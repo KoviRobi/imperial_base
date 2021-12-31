cd08c2b60b2e57ddd2d516e9f9938a812d2ab3e2
Kovacsics Robert <kovirobi@gmail.com>
2021-12-31 04:43:37 +0000

    Some further tweaking
    Using the <sup> in inline text changed the line-height which was
    annoying. Also, I misunderstood `rem`, it doesn't take the 100% viewport
    height into account.

--- slides.js
+++ slides.js
@@ -5,12 +5,12 @@ for (let i = 0; i < sections.length; i++) {
   div.classList.add("nav");
 
   let prev = document.createElement("a");
-  prev.append("Previous");
+  prev.append("\u2190");
   if (i > 0) prev.href = `#${sections[i - 1].id}`;
   div.append(prev);
 
   let next = document.createElement("a");
-  next.append("Next");
+  next.append("\u2192");
   if (i < sections.length - 1) next.href = `#${sections[i + 1].id}`;
   div.append(next);
 
